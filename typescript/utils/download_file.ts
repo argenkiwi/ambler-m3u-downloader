3447cab89d3c179fac1d18e2201ad1f7ca2b61ee
Leandro Peralta <leandro.peralta@gethomesafe.com>
2026-04-21 07:15:37 +1200

    Typescript: remove url encoding from downloaded filename.

--- typescript/utils/download_file.ts
+++ typescript/utils/download_file.ts
@@ -2,7 +2,7 @@ import { basename } from "https://deno.land/std@0.224.0/path/mod.ts";
 
 export async function downloadFile(url: string, folder: string): Promise<string> {
   await Deno.mkdir(folder, { recursive: true });
-  const filename = basename(new URL(url).pathname);
+  const filename = decodeURIComponent(basename(new URL(url).pathname));
   const destPath = `${folder}/${filename}`;
   const response = await fetch(url);
   if (!response.ok) {
